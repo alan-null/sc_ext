e57598fd95cb2abd3b24ebdd91d813cc93f5915d
Alan Płócieniak <alan-null@outlook.com>
2016-04-28 23:47:00 +0200

    section switches rewritten

--- app/scripts.ts/sc_ext.ts
+++ app/scripts.ts/sc_ext.ts
@@ -1,5 +1,6 @@
 'use strict';
 declare var scExt: any;
+declare var scSitecore: any;
 
 interface ISitecoreExtensionsModule {
     moduleName: string;
@@ -34,14 +35,84 @@ class DatabaseNameModule extends BaseModule implements ISitecoreExtensionsModule
         startButton.insertBefore(dbnameDiv, startButton.firstChild);
     }
 
-    canExecute() {
+    canExecute(): boolean {
         return true;
     }
 
-    initialize() {
+    initialize(): void {
         this.adDbNameToHeader(this.getDbName().toUpperCase());
     }
 }
+
+class SectionSwitchesModule extends BaseModule implements ISitecoreExtensionsModule {
+    sectionSwitchButtonClassName: string;
+    constructor(name: string, description: string) {
+        super(name, description);
+        this.sectionSwitchButtonClassName = 'scEButton';
+    }
+
+    closeOpenedSections() {
+        scExt.htmlHelpers.triggerEventOnSelector('.scEditorSectionCaptionExpanded .scEditorSectionCaptionGlyph', 'click');
+    };
+
+    openClosedSections() {
+        scExt.htmlHelpers.triggerEventOnSelector('.scEditorSectionCaptionCollapsed .scEditorSectionCaptionGlyph', 'click');
+    };
+
+    createTabControlButton(text: string, callback: Function): HTMLAnchorElement {
+        var span = scExt.htmlHelpers.createElement('span', {});
+        span.innerText = text
+        var link = scExt.htmlHelpers.createElement('a', {
+            href: '#',
+            class: 'scEditorHeaderNavigator scEditorHeaderButton scButton ' + this.sectionSwitchButtonClassName
+        });
+        link.onclick = callback
+        link.appendChild(span);
+        return link;
+    }
+
+    private insertButtons(): void {
+        var btnCollapse = this.createTabControlButton('Collapse', this.closeOpenedSections)
+        var btnExpand = this.createTabControlButton('Expand', this.openClosedSections)
+
+        var controlsTab = document.querySelector('.scEditorTabControlsTab5');
+        controlsTab.insertBefore(btnCollapse, controlsTab.firstChild);
+        controlsTab.insertBefore(btnExpand, controlsTab.firstChild);
+    };
+
+    buttonsExists(): boolean {
+        return document.getElementsByClassName(this.sectionSwitchButtonClassName).length > 0
+    }
+
+    refreshButtons(): void {
+        if (!this.buttonsExists()) {
+            this.insertButtons();
+        }
+    }
+
+    addTreeNodeHandlers(className: string): void {
+        var nodes = document.getElementsByClassName(className);
+        for (var i = 0; i < nodes.length; i++) {
+            nodes[i].addEventListener('click', function (evt) {
+                setTimeout(() => {
+                    this.refreshButtons();
+                }, 10);
+            });
+        }
+    }
+
+    canExecute(): boolean {
+        return true;
+    }
+
+    initialize(): void {
+        window.addEventListener('load', () => this.insertButtons());
+        this.addTreeNodeHandlers('scContentTree');
+        scExt.htmlHelpers.addProxy(scSitecore, 'postEvent', () => setTimeout(this.refreshButtons, 10)
+        )
+    }
+}
+
 
 class SitecoreExtensions {
     modules: ISitecoreExtensionsModule[];
@@ -74,4 +145,5 @@ class SitecoreExtensions {
 
 var sce = new SitecoreExtensions();
 sce.addModule(new DatabaseNameModule('Database Name', 'Displays current database name in the Content Editor header'));
+sce.addModule(new SectionSwitchesModule('Section Switches', 'Easily open/close all item sections with just one click'));
 sce.initModules();
