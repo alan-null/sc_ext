5bd97f191385780127b8d16d9a8035887daf66b3
Alan Płócieniak <alan-null@outlook.com>
2016-11-21 02:05:55 +0100

    #63 | Add option to reset links in reset settings page

--- app/options/controllers/SettingsController.ts
+++ app/options/controllers/SettingsController.ts
@@ -5,19 +5,31 @@ module SitecoreExtensions.Options {
 
     export class SettingsController extends BaseOptionsController {
         constructor($scope: any, formlyVersion: string) {
-            super($scope, formlyVersion, 'General');
+            super($scope, formlyVersion, 'Settings');
             $scope.vm.title = 'Settings';
             $scope.vm.reset = this.reset;
         }
 
         getFields() {
-            return [];
+            return [
+                {
+                    key: 'resetLinks',
+                    type: 'checkbox',
+                    defaultValue: true,
+                    templateOptions: {
+                        label: 'Reset links'
+                    },
+                }
+            ];
         }
 
         reset() {
             let confirmation = confirm("Are you sure you want to reset all Sitecore Extensions options?");
             if (confirmation) {
                 chrome.storage.local.clear();
+            }
+            if(this.model.resetLinks){
+                localStorage.clear();
             }
         }
     }
