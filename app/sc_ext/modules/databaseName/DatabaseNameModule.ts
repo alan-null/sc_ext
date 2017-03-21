7e6a8da89b7b84725d908fadd725624449e58872
Alan Płócieniak <alan-null@outlook.com>
2017-03-21 17:45:17 +0100

    #83 | Change database name when context database has changed

--- app/sc_ext/modules/databaseName/DatabaseNameModule.ts
+++ app/sc_ext/modules/databaseName/DatabaseNameModule.ts
@@ -1,6 +1,8 @@
 /// <reference path='../../_all.ts'/>
 
 namespace SitecoreExtensions.Modules.DatabaseName {
+    import DatabaseChangeEventArgs = Events.DatabaseChangeEventArgs;
+
     export class DatabaseNameModule extends ModuleBase implements ISitecoreExtensionsModule {
         constructor(name: string, description: string, rawOptions: Options.ModuleOptionsBase) {
             super(name, description, rawOptions);
@@ -13,16 +15,30 @@ namespace SitecoreExtensions.Modules.DatabaseName {
         initialize(): void {
             var dbName = Context.Database();
             if (dbName != null) {
-                this.adDbNameToHeader(dbName.toUpperCase());
+                this.addDbNameToHeader(dbName.toUpperCase());
             }
+
+            new Events.EventHandler("onDatabaseChange", (args: DatabaseChangeEventArgs) => { this.onDatabaseChange(args); });
         }
 
-        adDbNameToHeader(dbName: string): void {
+        onDatabaseChange(args: DatabaseChangeEventArgs) {
+            this.removeDbNameFromHeader();
+            this.addDbNameToHeader(args.databaseName.toUpperCase());
+        }
+
+        addDbNameToHeader(dbName: string): void {
             var dbnameDiv = HTMLHelpers.createElement<HTMLDivElement>('div', { class: 'sc-ext-dbName' });
             dbnameDiv.innerText = dbName;
 
             let destination = document.querySelector('.sc-globalHeader');
             destination.insertBefore(dbnameDiv, destination.firstChild);
+        }
+
+        removeDbNameFromHeader(): void {
+            let element = document.querySelector('.sc-ext-dbName');
+            if (element) {
+                element.remove();
+            }
         }
     }
 }
