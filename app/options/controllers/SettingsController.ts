38509a4d5a8d065c457bda1516ea5178ad4ffc17
Alan Płócieniak <alan-null@outlook.com>
2017-03-23 10:44:31 +0100

    #87 | Links removed after reverting settings to defaults

--- app/options/controllers/SettingsController.ts
+++ app/options/controllers/SettingsController.ts
@@ -35,12 +35,26 @@ module SitecoreExtensions.Options {
             let confirmation = confirm("Are you sure you want to reset all Sitecore Extensions options?");
             if (confirmation) {
                 chrome.storage.local.clear();
-            }
-            if (this.model.resetLinks) {
-                localStorage.removeItem('Links');
-            }
-            if (this.model.resetDbColorsMapping) {
-                localStorage.removeItem('Database Colour');
+
+                let provider = new Options.OptionsProvider();
+                let options = [];
+                if (this.model.resetLinks) {
+                    localStorage.removeItem('Links');
+                } else {
+                    options.push(new ModuleOptionsBase('Links', new LinkStorage().get()));
+                }
+
+                if (this.model.resetDbColorsMapping) {
+                    localStorage.removeItem('Database Colour');
+                } else {
+                    options.push(new ModuleOptionsBase('Database Colour', {
+                        enabled: true,
+                        colors: new DatabasesColorsStorage().get()
+                    }));
+                }
+                if (options.length > 0) {
+                    provider.setOptions(options, () => { });
+                }
             }
         }
     }
