f69fbb6e520f6e3e66b999c485784653d71929a8
Alan Płócieniak <alan-null@outlook.com>
2022-10-02 21:45:10 +0200

    #147 | Migrate to manifest v3

--- app/chrome/background.ts
+++ app/chrome/background.ts
@@ -9,28 +9,32 @@ chrome.runtime.onInstalled.addListener(details => {
 });
 
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+  if (sender.tab == null) {
+    console.log("Tab cannot be null");
+    return;
+  }
   if (request.sc_ext_setBadgeText_request) {
-    chrome.browserAction.setBadgeText({ tabId: sender.tab.id, text: request.modulesCount });
+    chrome.action.setBadgeText({ tabId: sender.tab.id, text: request.modulesCount });
   }
   if (request.sc_ext_setIcon_request) {
-    chrome.browserAction.setIcon({
-      path: request.newIconPath,
+    chrome.action.setIcon({
+      path: chrome.runtime.getURL(request.newIconPath),
       tabId: sender.tab.id
     });
   }
-  chrome.browserAction.setPopup({
+  chrome.action.setPopup({
     tabId: sender.tab.id,
     popup: 'chrome/popup/popup.html'
   });
-  chrome.browserAction.setBadgeBackgroundColor({ color: '#666' });
+  chrome.action.setBadgeBackgroundColor({ color: '#666' });
 
   if (request.sc_ext_checkVersion_request) {
     console.log(chrome.runtime.getManifest().version);
     console.log(request.version);
 
     if (request.version == null || request.version != chrome.runtime.getManifest().version) {
-      chrome.browserAction.setBadgeText({ tabId: sender.tab.id, text: "NEW", });
-      chrome.browserAction.setPopup({
+      chrome.action.setBadgeText({ tabId: sender.tab.id, text: "NEW", });
+      chrome.action.setPopup({
         tabId: sender.tab.id,
         popup: 'chrome/popup/popup-new-version.html'
       });
